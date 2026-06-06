94c764408e656569e48a9430e8876cf9f65bfe91
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-06 19:58:40 +0100

    Create index.ts to export Paystack modules

---
+++ packages/providers/paystack/src/index.ts
@@ -0,0 +1,11 @@
+export { PaystackProvider } from './paystack.provider';
+export type {
+  PaystackInitializeResponse,
+  PaystackVerifyResponse,
+  PaystackRefundResponse,
+  PaystackWebhookPayload,
+} from './types';
+export {
+  PAYSTACK_EVENTS,
+  PAYSTACK_SIGNATURE_HEADER,
+} from './constants';
