d862ef20ecb74da7fbe92f35352e63aec2224295
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-06 20:12:53 +0100

    Create index.ts to export Flutterwave modules

---
+++ packages/providers/flutterwave/src/index.ts
@@ -0,0 +1,11 @@
+export { FlutterwaveProvider } from './flutterwave.provider';
+export type {
+  FlutterwaveInitializeResponse,
+  FlutterwaveVerifyResponse,
+  FlutterwaveRefundResponse,
+  FlutterwaveWebhookPayload,
+} from './types';
+export {
+  FLUTTERWAVE_EVENTS,
+  FLUTTERWAVE_SIGNATURE_HEADER,
+} from './constants';
