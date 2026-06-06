be475417fae45c7f28200c3a62d2fe60ec8f047a
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-07 00:39:29 +0100

    Add index.ts to export Interswitch modules and types

---
+++ packages/providers/interswitch/src/index.ts
@@ -0,0 +1,12 @@
+export { InterswitchProvider } from './interswitch.provider';
+export type {
+  InterswitchEnvironment,
+  InterswitchTokenResponse,
+  InterswitchPurchaseResponse,
+  InterswitchQueryResponse,
+  InterswitchWebhookPayload,
+} from './types';
+export {
+  INTERSWITCH_SIGNATURE_HEADER,
+  INTERSWITCH_STATUS_CODES,
+} from './constants';
